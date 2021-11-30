f4c6703d799373207e9b9bd0c30023c4e1ff7264
OlegTch <Olgard984@gmail.com>
2021-11-30 17:18:31 +0200

    change structure

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Statistics from "./components/statistics/Statistics";
 import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
-import user from "./components/profile/user.json";
-import data from "./components/statistics/data.json";
-import friends from "./components/FriendList/friends.json";
-import transactions from "./components/TransactionHistory/transactions.json";
+import user from "./data/user.json";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
 
 console.log("profile");
 
